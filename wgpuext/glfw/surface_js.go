e05157a0a6ac12c6d70d2657072b999ed76cd5f0
Kai O'Reilly <koreilly5297@gmail.com>
2024-07-27 15:51:48 -0700

    remove unnecessary js wgpuext/glfw stub

--- wgpuext/glfw/surface_js.go
+++
@@ -1,9 +0,0 @@
-//go:build js
-
-package wgpuext_glfw // import "github.com/rajveermalviya/go-webgpu/wgpuext/glfw"
-
-import "github.com/rajveermalviya/go-webgpu/wgpu"
-
-func GetSurfaceDescriptor(any) *wgpu.SurfaceDescriptor { // placeholder stub for js
-	return &wgpu.SurfaceDescriptor{}
-}
