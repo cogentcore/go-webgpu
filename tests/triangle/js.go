939a9b01c779af9dada9d272bdafe153663757eb
Kai O'Reilly <koreilly5297@gmail.com>
2024-07-27 16:11:33 -0700

    call Render in triangle example on js

--- tests/triangle/js.go
+++ tests/triangle/js.go
@@ -2,16 +2,24 @@
 
 package main
 
-import "github.com/rajveermalviya/go-webgpu/wgpu"
+import (
+	"time"
+
+	"github.com/rajveermalviya/go-webgpu/wgpu"
+)
 
 type window struct{}
 
 func (w window) GetSize() (int, int) { return 512, 512 }
 
 func main() {
-	_, err := InitState(&window{}, &wgpu.SurfaceDescriptor{})
+	s, err := InitState(&window{}, &wgpu.SurfaceDescriptor{})
 	if err != nil {
 		panic(err)
 	}
-	select {}
+	defer s.Destroy()
+	ticker := time.NewTicker(time.Second / 60)
+	for range ticker.C {
+		s.Render()
+	}
 }
